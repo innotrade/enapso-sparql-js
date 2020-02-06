d85ce63dd17ff414dd61d645277918822c49d324
Alexander Schulze <a.schulze@innotrade.de>
2020-02-06 22:01:59 +0100

    Clean-up, preparation for implementation of JS methods for easy graph data management.

--- demo/demo.js
+++ demo/demo.js
@@ -3,9 +3,9 @@
 // Author: Alexander Schulze
 
 // require the Enapso GraphDB Client
-const EnapsoGraphDBClient = require("enapso-graphdb-client");
+const EnapsoGraphDBClient = require("@innotrade/enapso-graphdb-client");
 // require the Enapso SPARQL JS
-const EnapsoSPARQLJS = require("../lib/enapso-sparql-js");
+const { enspjs } = require("../index");
 
 // connection data to the running GraphDB instance
 const
@@ -24,60 +24,12 @@ const GRAPHDB_DEFAULT_PREFIXES = [
 
 console.log("Enapso SPARQL JS Demo");
 
-const EnapsoSPARQLJSDemo = {
+class EnapsoSPARQLJSDemo {
 
-	graphDBEndpoint: null,
-	authentication: null,
-
-	createEndpoint: async function () {
-		// instantiate a new GraphDB endpoint
-		return new EnapsoGraphDBClient.Endpoint({
-			baseURL: GRAPHDB_BASE_URL,
-			repository: GRAPHDB_REPOSITORY,
-			prefixes: GRAPHDB_DEFAULT_PREFIXES
-		});
-	},
-
-	login: async function () {
-		// login into GraphDB using JWT
-		let lRes = await this.graphDBEndpoint.login(
-			GRAPHDB_USERNAME,
-			GRAPHDB_PASSWORD
-		);
-		return lRes;
-	},
-
-	demoGetClassSchema: async function (aOptions) {
-		var lRes = await EnapsoSPARQLJS.getClassSchema({
-			classIRI: "http://ont.enapso.com/test#Person",
-			context: "http://ont.enapso.com/test"
-		});
-		console.log("\nClass Schema: " + JSON.stringify(lRes, null, 2));
-	},
-
-	demoGetIndividuals: async function (aOptions) {
-		var lRes = await EnapsoSPARQLJS.getIndividuals({
-			classIRI: "http://ont.enapso.com/test#Person",
-			context: "http://ont.enapso.com/test"
-		});
-		console.log("\nIndividuals: " + JSON.stringify(lRes, null, 2));
-	},
-
-	demo: async function () {
-		this.graphDBEndpoint = await this.createEndpoint();
-		this.authentication = await this.login();
-		// verify authentication
-		if (!this.authentication.success) {
-			console.log("\nLogin failed:\n" +
-				JSON.stringify(this.authentication, null, 2));
-			return;
-		}
-		console.log("\nLogin successful");
-		EnapsoSPARQLJS.mGraphDBEndpoint = this.graphDBEndpoint;
-
-		await this.demoGetClassSchema();
-		await this.demoGetIndividuals();
+	async demo() {
+		console.log('Hello World!');
 	}
+
 }
 
-EnapsoSPARQLJSDemo.demo();
+new EnapsoSPARQLJSDemo().demo();
